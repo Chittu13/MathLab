b1ee92b118adf358dfac66b9940e7100bbddb240
Chittu13 <shanmukha.s.royal@gmail.com>
2022-11-10 16:17:18 +0530

    Updated code :)

---
+++ MATHLAB/Mathlab.m
@@ -0,0 +1,21 @@
+2D wave form
+
+t=-pi:0.1:pi;
+x=sin(t)
+plot(t,x)
+title('sinewave','fontsize',20);
+xlable('amplitude','fontsize',20);
+ylable('time','fontsize',20);
+
+
+
+3D wave form
+
+
+t=-pi:0.1:pi;
+x=sin(t);
+y=x.*sin(t);
+plot3(t,x,y)
+title('sinewave');
+xlable('amplitude');
+ylable('time');
