d52098a620269c2804dce0a7129f7f0281e64106
K. Shanmukha Srinivasulu Royal <114333116+Chittu13@users.noreply.github.com>
2022-12-20 16:00:13 +0530

    Create partice.m

---
+++ MATHLAB/partice.m
@@ -0,0 +1,24 @@
+a=[1 2 3 4; 4 5 6 7; 7 8 9 10];
+% Enter the code
+% 1.a(:,2) second column of a
+% 2.a(:,2:3) second column to third column of a
+% 3.a(2:3,2:3) second and third rows and second and third columns
+
+ % do
+% row 1, colum 2;
+% all rows, coloum 1,2;
+
+% ones(4,3)  first varible tell no.of row and second varible tells no.of coloums
+% eye(4) horizontal 1's will come.
+% zeros(5) all zero's in a matrix will be done.
+
+
+
+% rand(3,5) the reand() function creates an array of random number between
+% 0 to 1.
+
+
+
+% Multidimentsional Arrays
+% The array a is a 3-by-3 arrayl; we can add a third dimension to a, by providing the values like-\
+% a(:,:,2)=[1 2 3; 4 5 6; 7 8 9]
